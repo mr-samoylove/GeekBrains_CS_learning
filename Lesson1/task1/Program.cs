b2762bb969ac82c4ff93ff576a4d2dffc7a1ea5c
psina <mr.samoylove@gmail.com>
2022-06-23 08:43:23 +0300

    task1 & task3 done

---
+++ Lesson1/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+Console.WriteLine("Эта программа проверяет, является ли первое число квадратом второго.\nВведите первое число и нажмите Enter");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число и нажмите Enter");
+int second = Convert.ToInt32(Console.ReadLine());
+
+if (second * second == first)
+{
+    Console.WriteLine($"Да, первое число - это квадрат второго: {second} * {second} = {first}");
+}
+else
+{
+    Console.WriteLine($"Нет, первое число не является квадратом второго: {second} * {second} != {first}");
+}
---
+++ Lesson1/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Эта программа выдает название дня недели по заданному номеру.\nВведите цифру от 1 до 7 и нажмите Enter");
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch (day)
+{
+    case 1: Console.WriteLine("Понедельник"); break;
+    case 2: Console.WriteLine("Вторник"); break;
+    case 3: Console.WriteLine("Среда"); break;
+    case 4: Console.WriteLine("Четверг"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота"); break;
+    case 7: Console.WriteLine("Воскресенье"); break;
+    default: Console.WriteLine("Это не цифра от 1 до 7"); break;
+}
