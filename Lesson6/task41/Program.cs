49cebb201bb25cc55570d91773c45327d7e65cb1
psina <mr.samoylove@gmail.com>
2022-07-11 21:11:40 +0300

    Lesson 6 homework

---
+++ Lesson6/task41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.WriteLine("Эта программа считает, сколько чисел больше 0 ввёл пользователь.");
+Console.Write("Сколько чисел вы хотите ввести? ");
+uint n = Convert.ToUInt32(Console.ReadLine());
+
+uint counter = 0;
+int value = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"Введите {i}-е число: ");
+    value = Convert.ToInt32(Console.ReadLine());
+    if (value > 0) counter++;
+}
+
+Console.WriteLine($"Количество чисел больше нуля = {counter}");
---
+++ Lesson6/task43/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// y = 5 * x + 2
+// y = 9 * x + 4
+
+Console.WriteLine("Эта программа находит точку пересечения двух прямых, заданных уравнениями\n\ty = k1 * x + b1\n\ty = k2 * x + b2");
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+// k1 * x + b1 = k2 * x + b2
+// k1 * x – k2 * x = b2 – b1
+// x(k1 – k2) = b2 – b1
+// x = (b2 – b1) / (k1 – k2)
+
+if (k1 == k2)
+    if (b1 == b2) Console.WriteLine("Прямые равны");
+    else Console.WriteLine("Пересечений нет, линии параллельны");
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    Console.WriteLine($"Пересечение в ({x}, {y})");
+}
+
+
